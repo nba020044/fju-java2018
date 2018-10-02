e47c472c72064da5283a20d44583eb76ec77e782
yoyo <user@es302n20>
2018-10-02 10:37:28 +0800

    variable

--- yoyoproject/src/com/yoyo/Hello.java
+++ yoyoproject/src/com/yoyo/Hello.java
@@ -5,6 +5,14 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("Hello world");
+ int age = 28;
+ int height = 165;
+ int weight = 52; 
+ int schoolYear = 107;
+ System.out.println(age);
+ System.out.println(height);
+ System.out.println(weight);
+ System.out.println(schoolYear);
 	}
 
 }
