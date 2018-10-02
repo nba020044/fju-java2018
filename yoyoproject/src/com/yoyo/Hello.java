0bb702a64c55a79c9ec869b325e41b7f6cc0be4d
yoyo <user@es302n20>
2018-10-02 11:05:47 +0800

    exam 2

--- yoyoproject/src/com/yoyo/Hello.java
+++ yoyoproject/src/com/yoyo/Hello.java
@@ -9,10 +9,17 @@ public class Hello {
  int height = 165;
  int weight = 52; 
  int schoolYear = 107;
+ int mood = 100;
+ int weather = 80;
+ int breakfast = 65;
+ int dinner = 0;
  System.out.println(age);
  System.out.println(height);
  System.out.println(weight);
- System.out.println(schoolYear);
+ System.out.println(mood);
+ System.out.println(weather);
+ System.out.println(breakfast);
+ System.out.println(dinner);
 	}
 
 }
