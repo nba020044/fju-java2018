e81a780a95c24c523557fb81b07c71f94be77641
yoyo <user@es302n20>
2018-10-02 11:00:50 +0800

    exam exam

---
+++ exam/src/com/fju/Hello.java
@@ -0,0 +1,19 @@
+package com.fju;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int age = 19;
+		 int birthday = 19990908;
+		 int height = 163;
+		 int weight = 52;
+		 int schoolYear = 107;
+				 System.out.println(age);
+		 System.out.println(birthday);
+		 System.out.println(height);
+		 System.out.println(weight);
+		 System.out.println(schoolYear);
+	}
+
+}
