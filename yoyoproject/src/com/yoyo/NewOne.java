63af5a7c23b64022963bb13f099307c10b9c0bac
user <user@es302n20>
2018-10-30 12:00:50 +0800

    1030

---
+++ yoyoproject/src/com/yoyo/NewOne.java
@@ -0,0 +1,39 @@
+package com.yoyo;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NewOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n = 100/3;
+int m = 100%3;
+System.out.println(n + "\t" + m);
+int n2 = (int)(8/7.0);
+System.out.println(n2);
+System.out.printf("Four thirds = %.5f" , 4.0/3.0);
+String name = "Yoyo";
+int age = 19;
+System.out.printf("Hi, %s, Your age: %d\n", name, age);
+
+double c = 24;
+double f = ((c*9)/5+32);
+System.out.println(f);
+
+/*System.out.println("Type a temperature");
+Scanner scanner = new Scanner(System.in);
+double C= scanner.nextDouble();
+double F = C * (9/5.0)+ 32;
+System.out.println(F);*/
+
+Random random = new Random();
+
+int a = 1;
+while (a > 0){
+System.out.println(123456);
+a = a + 1;}
+
+	}
+
+}
