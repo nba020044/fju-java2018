c76767fab585d8cba7d4a9f3f5474c75d8fc6e5c
user <user@es302n14>
2018-12-18 11:58:57 +0800

    1218day

--- yoyoproject/src/com/yoyo/SignUp.java
+++ yoyoproject/src/com/yoyo/SignUp.java
@@ -6,7 +6,7 @@ public class SignUp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Are you 18?(y/N)");
+		/*System.out.println("Are you 18?(y/N)");
 		Scanner scanner = new Scanner(System.in);
 		String line = scanner.nextLine();
 		boolean adult = line.toUpperCase().equals("Y");
@@ -25,10 +25,38 @@ public class SignUp {
 			
 		
 		} else {
-			System.out.println("Thanks for supporting us, bye!");
+			System.out.println("Thanks for supporting us, bye!"); */
+	String s = "Hello world";
+	//          01234567890
+	String s2 ="Jack Tom Eric";
+	//			0123456789012
+	System.out.println();
+	System.out.println(s2.lastIndexOf("c"));
+	//S2字串內ㄉTOM"都"改成BOSS，所以裡面有兩個tomㄉ話就要全部都改成boss
+	//Jack boss Eric
+	String name ="Tom";
+	//name = name.replace("Tom", "boss");
+	System.out.println(s2.substring(0, 5) + name + s2.substring(8, 13));
+	int index = s2.indexOf(name);
+	System.out.println(s2.substring(0, index) + "boss" +
+			s2.substring(index+name.length()));
+	//(s2.substring(0, 4)) ,name,s2.substring(8 , 12)
+	//System.out.println(s == s2);
+	//System.out.println(s.equals(s2));
+	System.out.println(s.substring(4)); 
+	System.out.println(s.substring(2 , 6));
+	int length = s.length();
+	char last = s.charAt(length - 1);
+	String r = "";
+	for (int i = s.length() - 1; i >= 0; i--) {
+		System.out.print(s.charAt(i));
+
 		}
-		
+
+		}
+	
 		
 	}
 
-}
+
+
